b1464411d6eb53fcf02b0e60f47ac04b5d0c4ba1
Giga Kiphiani <146529069+Giga069@users.noreply.github.com>
2024-09-27 18:55:58 +0400

    Add files via upload

---
+++ day20/daveleba20.py
@@ -0,0 +1,30 @@
+                        #დავალება N1
+# def find_needle(haystack):
+#     needle_index = haystack.index("needle")
+#     return("i found the needle at position") + str(needle_index)
+
+                        #დავალება N2
+# def make_upper_case(s):
+#     input(str)
+#     return s.upper()
+                        #დავალება N3
+# def sum_array(a):
+#     if a == 0:
+#         print(0)
+#     else:
+#         print (sum(a))
+                        #დავალება N4
+# def are_you_playing_banjo(name):
+#     if name.lower().starts("r"):
+#         return name + " plays banjo"
+#     elif name.higher().starts("R"):
+#         return name + "plaus banjo"
+#     else:
+#         return name + " does not play banjo"
+                        #დავალება N5
+# def invert(list):
+#     return [-num for num in list]
+                        #დავალება N6 (ვერ გავიგე.)
+# def find_average(numbers):
+#     return sum(numbers) %2
+
